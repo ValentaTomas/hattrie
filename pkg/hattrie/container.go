c8043649d1ca787445267b72c5eb7e604b817ea9
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-01-28 15:54:23 +0100

    Finish container tests

---
+++ pkg/hattrie/container.go
@@ -0,0 +1,79 @@
+package hattrie
+
+import (
+	"github.com/yourbasic/radix"
+)
+
+// ValueType is an alias for values in the array hash.
+type ValueType = uint64
+
+// pairs is a type definition for string to uint64 map.
+//
+// The map in Go is implemented with buckets as arrays with a fixed length 8 and a factor of 6.5.
+// According to the implemetation https://github.com/golang/go/blob/master/src/runtime/map.go,
+// the key and values are saved in separate arrays, instead of alternating length-key-value in one array, as in the C implementation mentioned further down.
+//
+// I haven't tried using the exact implementation from the HAT-trie C lib (https://github.com/dcjones/hat-trie/blob/master/src/ahtable.h) yet.
+// That would require using unsafe pointers directly and I want to avoid that until I know that this is a performance bottleneck.
+//
+// All other custom array hash implementations that I tried were slower that the default Go map for the 1 to 16384 entries that the array hash should handle before the trie node burst.
+//
+// TODO: By not using pointers in the map the GC may be able to skip it, but the string as key may be preventing this.
+// TODO: Should we limit the maximum key size so we can use byte arrays as keys?
+type pairs map[string]ValueType
+
+type trieContainer struct {
+	pairs
+	hybrid     bool
+	splitStart byte
+	splitEnd   byte
+}
+
+func newTrieContainer(size int) *trieContainer {
+	// TODO: Optimize the ideal starting and max size.
+	return &trieContainer{
+		pairs:    make(pairs, size),
+		splitEnd: byteMaxValue,
+	}
+}
+
+// SortedKeys returns a slice of hash's keys, sorted lexicographically with the radix sort.
+// This slice could be then used for iterating the hash in order.
+func (c *trieContainer) SortedKeys() []string {
+	// We can preallocate the slice to prevent resizing for each key.
+	keys := make([]string, 0, len(c.pairs))
+
+	// TODO: Try iterating while retrieving the actual values and sorting them?
+	for k := range c.pairs {
+		keys = append(keys, k)
+	}
+
+	// Radix sort should work well with the HAT-trie, because even though the sorting needs additional space,
+	// we are only sorting one array hash at the time, so the space requirement increase should be constant (at worst for 16384 - max hash table size before burst).
+	//
+	// Iterating over 2^14 elements in the map in order takes 2992016 ns for the radix sort vs 4705317 ns for the default sort.String.
+	//
+	// TODO: Add tests to check if the radix sorts in the required order (lexicoghraphic?).
+	// TODO: Test the radix sort vs. quicksort on more representative data.
+	radix.Sort(keys)
+
+	return keys
+}
+
+func (c *trieContainer) Put(key string, prefixIdx int, value ValueType) {
+	// TODO: []byte to string conversion in L-value map element index expressions may allocate - beware and test this if we are changing the keys to []byte.
+	// TODO: If we need "upserting" put — change value type to *uint64 and handle upsert by retrieving the value once and cheking for nil. Can we do the upsert with some combination of assignment/bitwise operations?
+	if c.hybrid {
+		if prefixIdx == 0 {
+			c.pairs[key] = value
+		} else {
+			c.pairs[key[prefixIdx:]] = value
+		}
+	} else {
+		if prefixIdx == 0 {
+			c.pairs[key[1:]] = value
+		} else {
+			c.pairs[key[prefixIdx:]] = value
+		}
+	}
+}
---
+++ pkg/hattrie/container_test.go
@@ -0,0 +1,150 @@
+package hattrie
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+// TODO: Use assert
+
+const containerSize = maxContainerSizeBeforeBurst
+
+// TODO: Test several lengths of keys or just use a proper dictionary.
+func getKey(i ValueType) string {
+	return fmt.Sprintf("%d-%d", i, i)
+	// return fmt.Sprintf("%d%d%d%d%d%d", i, i, i, i, i, i)
+}
+
+// func TestTrieContainerPut(t *testing.T) {
+// 	c := newTrieContainer(initialContainerSize)
+
+// 	c.Put(getKey(1), 0, 1)
+
+// 	c.Put(getKey(2), 1, 2)
+
+// 	if c.pairs[getKey(1)[0+1:]] != 1 {
+// 		t.Errorf("Wrong value retrieved for prefix 0: key %v", getKey(1)[0:])
+// 	}
+
+// 	if c.pairs[getKey(2)[1+1:]] != 2 {
+// 		t.Errorf("Wrong value retrieved for prefix 1: key %v", getKey(2)[1:])
+// 	}
+// }
+
+func TestTrieContainerPutHybrid(t *testing.T) {
+	c := newTrieContainer(initialContainerSize)
+
+	c.hybrid = true
+
+	c.Put(getKey(1), 0, 1)
+
+	c.Put(getKey(2), 1, 2)
+
+	if c.pairs[getKey(1)[0:]] != 1 {
+		t.Errorf("Wrong value retrieved for prefix 0: key %v", getKey(1)[0:])
+	}
+
+	if c.pairs[getKey(2)[1:]] != 2 {
+		t.Errorf("Wrong value retrieved for prefix 1: key %v", getKey(2)[1:])
+	}
+}
+
+func TestTrieContainerSortedKeys(t *testing.T) {
+	c := newTrieContainer(initialContainerSize)
+	pairs := []struct {
+		key   string
+		value ValueType
+	}{
+		{"c", 3},
+		{"a", 1},
+		{"b", 2},
+	}
+
+	for _, pair := range pairs {
+		value := pair.value
+		c.pairs[pair.key] = value
+	}
+
+	sortedKeys := c.SortedKeys()
+	sorted := make([]struct {
+		key   string
+		value ValueType
+	}, 0, len(sortedKeys))
+
+	for _, key := range sortedKeys {
+		value := c.pairs[key]
+
+		sorted = append(sorted, struct {
+			key   string
+			value ValueType
+		}{
+			key:   key,
+			value: value,
+		})
+	}
+
+	if !sort.SliceIsSorted(sorted, func(i, j int) bool {
+		return sorted[i].value < sorted[j].value
+	}) {
+		t.Errorf("SortedKeys didn't sort the keys correctly: %+v", sorted)
+	}
+}
+
+func BenchmarkTrieContainerPut(b *testing.B) {
+	b.StopTimer()
+	c := newTrieContainer(initialContainerSize)
+	// Start with the half of the maximum key-value pairs
+	for i := 0; i < containerSize/2; i++ {
+		value := ValueType(i)
+		// c.pairs[getKey(value)] = value
+		c.Put(getKey(value), 0, value)
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		value := ValueType(i)
+		// c.pairs[getKey(value)] = value
+		c.Put(getKey(value), 0, value)
+	}
+
+	b.ReportAllocs()
+}
+
+func BenchmarkTrieContainerSortedIterate(b *testing.B) {
+	b.StopTimer()
+	c := newTrieContainer(initialContainerSize)
+
+	for i := 0; i < containerSize; i++ {
+		value := ValueType(i)
+		c.pairs[getKey(value)] = value
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, k := range c.SortedKeys() {
+			_ = c.pairs[k]
+		}
+	}
+
+	b.ReportAllocs()
+}
+
+func BenchmarkTrieContainerIterate(b *testing.B) {
+	b.StopTimer()
+	c := newTrieContainer(initialContainerSize)
+
+	for i := 0; i < containerSize; i++ {
+		value := ValueType(i)
+		c.pairs[getKey(value)] = value
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, v := range c.pairs {
+			_ = v
+		}
+	}
+
+	b.ReportAllocs()
+}
